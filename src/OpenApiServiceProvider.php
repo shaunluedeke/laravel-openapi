5d713a57a0f96c7b1aca924418b385af570ae801
shaunluedeke <shaun.luedeke5520@gmail.com>
2025-07-30 19:03:55 +0200

    Make `getPathsFromConfig` static and update bindings to reflect the change. Simplify closure definitions by marking them static.

--- src/OpenApiServiceProvider.php
+++ src/OpenApiServiceProvider.php
@@ -23,11 +23,11 @@ class OpenApiServiceProvider extends ServiceProvider
     {
         $this->mergeConfigFrom(__DIR__.'/../config/openapi.php', 'openapi');
 
-        $this->app->bind(CallbacksBuilder::class, fn () => new CallbacksBuilder($this->getPathsFromConfig('callbacks')));
-        $this->app->bind(RequestBodiesBuilder::class, fn () => new RequestBodiesBuilder($this->getPathsFromConfig('request_bodies')));
-        $this->app->bind(ResponsesBuilder::class, fn () => new ResponsesBuilder($this->getPathsFromConfig('responses')));
-        $this->app->bind(SchemasBuilder::class, fn () => new SchemasBuilder($this->getPathsFromConfig('schemas')));
-        $this->app->bind(SecuritySchemesBuilder::class, fn () => new SecuritySchemesBuilder($this->getPathsFromConfig('security_schemes')));
+        $this->app->bind(CallbacksBuilder::class, fn () => new CallbacksBuilder(self::getPathsFromConfig('callbacks')));
+        $this->app->bind(RequestBodiesBuilder::class, fn () => new RequestBodiesBuilder(self::getPathsFromConfig('request_bodies')));
+        $this->app->bind(ResponsesBuilder::class, fn () => new ResponsesBuilder(self::getPathsFromConfig('responses')));
+        $this->app->bind(SchemasBuilder::class, fn () => new SchemasBuilder(self::getPathsFromConfig('schemas')));
+        $this->app->bind(SecuritySchemesBuilder::class, fn () => new SecuritySchemesBuilder(self::getPathsFromConfig('security_schemes')));
 
         $this->app->singleton(Generator::class, static fn (Application $app) => new Generator(
             config('openapi'),
@@ -63,8 +63,8 @@ class OpenApiServiceProvider extends ServiceProvider
         $this->loadRoutesFrom(__DIR__.'/../routes/api.php');
     }
 
-    private function getPathsFromConfig(string $type): array
+    public static function getPathsFromConfig(string $type): array
     {
-        return collect(config('openapi.locations.'.$type, []))->map(fn ($dir) => glob($dir, GLOB_ONLYDIR))->flatten()->unique()->toArray();
+        return collect(config('openapi.locations.'.$type, []))->map(static fn ($dir) => glob($dir, GLOB_ONLYDIR))->flatten()->unique()->toArray();
     }
 }
